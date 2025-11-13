1cb1e632693bd11413b9f8ec4fe28f30473265c9
BiGPiG <Bogdan.ars.15@yandex.ru>
2025-11-13 10:35:42 +0300

    fix

--- laba_6/consumer.c
+++ laba_6/consumer.c
@@ -5,12 +5,26 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <sys/time.h>
+
+void get_current_time_with_ms(char *buffer, size_t size) {
+    struct timeval tv;
+    struct tm *timeinfo;
+    
+    gettimeofday(&tv, NULL);
+    timeinfo = localtime(&tv.tv_sec);
+    
+    snprintf(buffer, size, "%02d:%02d:%02d.%03ld",
+             timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec,
+             tv.tv_usec / 1000);
+}
 
 int main() {
     char *fifo = "myfifo";
     char buffer[256];
     time_t rawtime;
     struct tm *timeinfo;
+    char time_buffer[64];
 
     int fd = open(fifo, O_RDONLY);
     if (fd == -1) {
@@ -25,7 +39,8 @@ int main() {
 
     time(&rawtime);
     timeinfo = localtime(&rawtime);
-    printf("Current time: %s", asctime(timeinfo));
+    get_current_time_with_ms(time_buffer, sizeof(time_buffer));
+    printf("Current time: %s (%s)\n", asctime(timeinfo), time_buffer);
     printf("Received: %s\n", buffer);
 
     unlink(fifo);
--- laba_6/producer.c
+++ laba_6/producer.c
@@ -6,12 +6,26 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <sys/time.h>
+
+void get_current_time_with_ms(char *buffer, size_t size) {
+    struct timeval tv;
+    struct tm *timeinfo;
+    
+    gettimeofday(&tv, NULL);
+    timeinfo = localtime(&tv.tv_sec);
+    
+    snprintf(buffer, size, "%02d:%02d:%02d.%03ld",
+             timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec,
+             tv.tv_usec / 1000);
+}
 
 int main() {
     char *fifo = "myfifo";
     char buffer[256];
     time_t rawtime;
     struct tm *timeinfo;
+    char send_time[64];
 
     if (mkfifo(fifo, 0666) && errno != EEXIST) {
         perror("Fifo creation error");
@@ -23,7 +37,11 @@ int main() {
 
     time(&rawtime);
     timeinfo = localtime(&rawtime);
-    snprintf(buffer, sizeof(buffer), "Time: %sPID: %d", asctime(timeinfo), getpid());
+
+    get_current_time_with_ms(send_time, sizeof(send_time));
+    snprintf(buffer, sizeof(buffer), 
+             "Producer PID: %d | Data sent: %s", 
+             getpid(), send_time);
 
     int fd = open(fifo, O_WRONLY | O_NONBLOCK);
     if (fd == -1) {
